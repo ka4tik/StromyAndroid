bda53d9dadf8fabfd3440b15df858649e619e8e7
Kartik Singal <ka4tik@gmail.com>
2015-06-05 16:30:07 -0400

    simple api call working

--- app/src/main/java/com/example/ka4tik/stromy/MainActivity.java
+++ app/src/main/java/com/example/ka4tik/stromy/MainActivity.java
@@ -2,38 +2,60 @@ package com.example.ka4tik.stromy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String api_key="d162e457e174aa85b8b40e85dc69ff75";
+        double latitude=37.8267;
+        double longitude=-122.423;
+        String forcastUrl = "https://api.forecast.io/forecast/"+api_key+"/"+latitude+","+longitude;
+
+        OkHttpClient client=new OkHttpClient();
+        final Request request=new Request.Builder()
+                .url(forcastUrl)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG,response.body().string());
+                    }
+                }catch (Exception e)
+                {
+                    Log.e(TAG,"Exception caught: ",e);
+                }
+
+
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
