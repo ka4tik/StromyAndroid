6c2ec20fc74d73619205edb6f4c40c5e65f4ecdd
Kartik Singal <ka4tik@gmail.com>
2015-06-06 10:28:21 -0400

    added icons

--- app/src/main/java/com/example/ka4tik/stromy/MainActivity.java
+++ app/src/main/java/com/example/ka4tik/stromy/MainActivity.java
@@ -17,6 +17,9 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
@@ -24,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private CurrentWeather mCurrentWeather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,25 +53,51 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onResponse(Response response) throws IOException {
 
+
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
                             alertUserAboutError();
                         }
 
-                    } catch (Exception e) {
+                    } catch (IOException e) {
                         Log.e(TAG, "Exception caught: ", e);
                     }
-
+                    catch (JSONException e){
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
 
                 }
             });
         }
         else {
             Toast.makeText(this,"Network is unavailable",Toast.LENGTH_LONG).show();
-
         }
+
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+
+
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setTimeZone(timezone);
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+
+        Log.d(TAG,currentWeather.getFormattedTime());
+
+        return currentWeather;
 
     }
 
